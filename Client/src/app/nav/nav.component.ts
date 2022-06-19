5302e0c5b524e7ac9c1ac2cccb9c1a163d04abe9
killsanexe <killsan.exe@gmail.com>
2022-06-19 20:08:36 +0300

    added navbar

--- Client/src/app/nav/nav.component.ts
+++ Client/src/app/nav/nav.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss', './nav.style.scss']
 })
 export class NavComponent implements OnInit {
+
+  @ViewChild("menuBtn") menuBtn: any;
+  menuOpen = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  changeMenu = () => this.menuOpen ? this.menuOpen = false : this.menuOpen = true
 
 }
