bafe9358908190a2670d522dff0bdf9dcee9924c
killsanexe <killsan.exe@gmail.com>
2022-07-13 18:24:12 +0300

    connected profile page to API

---
+++ Client/src/app/_models/transaction.ts
@@ -0,0 +1,9 @@
+export interface Transaction {
+    id: number
+    type: string
+    amount: number
+    user: any
+    userId: number
+    transactionDate: string
+    btcPrice: number
+}
---
+++ Client/src/app/_models/userParams.ts
@@ -0,0 +1,5 @@
+export class UserParams{
+    pageNumber = 1;
+    PageSize = 5;
+    showAll = false;
+}
