0f3f7b0b26357300e3c8bb76f72fb3ab3c1d1217
killsanexe <killsan.exe@gmail.com>
2022-07-19 21:03:28 +0300

    added crypto url to nav bar

--- Client/src/app/_modules/crypto.module.ts
+++ Client/src/app/_modules/crypto.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CryptoComponent } from '../crypto/crypto/crypto.component';
 import { FormsModule } from '@angular/forms';
+import { CryptoComponent } from '../crypto/crypto.component';
 
 
 
